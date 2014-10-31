4eb473cc769611de33463173515927d58e5a2a93
ksacek <kamil.sacek@navertica.com>
2014-10-31 16:46:29 +0100

    Added script to read input parameters from xml file. Parameters for the repository could be saved in the file and used for calling all other scripts without entering the parameters manually.

---
+++ Scripts/Get-NAVGITSetup.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.Synopsis
+   Read setup from the xml and return the object to pipe to be used for other scripts
+.DESCRIPTION
+   Read setup from the xml and return the object to pipe to be used for other scripts without setting their parameters directly
+
+    Setup file structure
+    <?xml version="1.0"?>
+    <Object>
+    	<Property Name="Server">sqlserver</Property>
+	    <Property Name="Database">sqldatabase</Property>
+	    <Property Name="Path">Objects</Property>
+	    <Property Name="Files">Objects\*.txt</Property>
+	    <Property Name="NAVIdePath">c:\Program Files (x86)\Microsoft Dynamics NAV\71\RoleTailored Client\</Property>
+	    <Property Name="NAVServicePath">c:\Program Files\Microsoft Dynamics NAV\71\Service\</Property>
+    </Object>
+.EXAMPLE
+   Get-NAVGITSetup setup.xml | Update-NAVApplicationFromTxt
+#>
+
+
+param (
+    [string]$SetupFile='setup.xml'
+)
+
+Begin {
+    function ConvertFrom-Xml($XML) {
+        foreach ($Object in @($XML.Object)) {
+            $PSObject = New-Object PSObject
+            foreach ($Property in @($Object.Property)) {
+                $PSObject | Add-Member NoteProperty $Property.Name $Property.InnerText
+            }
+            $PSObject
+        }
+    }
+    [xml]$xml=Get-Content $SetupFile
+    $setup = ConvertFrom-Xml $xml
+
+    $env:NAVIdePath = "$($setup.NavIdePath)"
+    $env:NAVServicePath = "$($setup.NAVServicePath)"
+
+    $setup
+}
