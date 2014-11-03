6fe67a21179f65d1985c4bfce1e036954d1bd4d7
ksacek <kamil.sacek@navertica.com>
2014-11-03 12:55:03 +0100

    Get Last Build process corrected to return correct path. Template changed to process the output of the script correctly.

--- Scripts/TFSBuild/Get-LastBuildFile.ps1
+++ Scripts/TFSBuild/Get-LastBuildFile.ps1
@@ -7,4 +7,4 @@ param
 $file = Get-ChildItem -Path $Folder -Filter $Filter -Recurse | Sort-Object -Property CreationTime -Descending | Select-Object -First 1
 
 
-Write-Output $file.FullName
+Write-Output "$($file.FullName)"
