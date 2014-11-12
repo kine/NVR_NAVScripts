77525805455f30887cd4a9ec4d0737e1c62a056f
KSACEK <kamil.sacek@navertica.com>
2014-11-12 16:01:46 +0100

    Get-NAVGITSetup used to set the environment variables based on the setup file. Could replace the property of the TFS build template...

--- Scripts/TFSBuild/New-NAVServer.ps1
+++ Scripts/TFSBuild/New-NAVServer.ps1
@@ -7,6 +7,10 @@ Param (
 	[String]$Database,
 	[String]$Instance
 )
+#Write-Host $PSScriptRoot
+if (Test-Path $TargetPath\src\setup.xml) {
+  . "$PSScriptRoot\..\Get-NAVGITSetup.ps1" -SetupFile "$TargetPath\src\setup.xml" | Out-Null
+}
 Import-Module CommonPSFunctions -DisableNameChecking
 Import-Module NVR_NAVScripts -DisableNameChecking
 Import-NAVAdminTool
