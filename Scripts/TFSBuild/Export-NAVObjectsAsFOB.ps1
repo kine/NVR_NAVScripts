c87ab958c311e69241f5e60909043871d5085a3b
KSACEK <kamil.sacek@navertica.com>
2015-02-19 10:02:33 +0100

    Export-NAVObjectsAsFOB script has LogFolder parameter to pass the target folder for LOG

--- Scripts/TFSBuild/Export-NAVObjectsAsFOB.ps1
+++ Scripts/TFSBuild/Export-NAVObjectsAsFOB.ps1
@@ -2,8 +2,10 @@ Param (
 	[String]$Server,
 	[String]$Database,
 	[String]$ResultFob,
-	[String]$NavIde
+	[String]$NavIde,
+	[String]$LogFolder
 )
 Import-Module NVR_NAVScripts -Force -DisableNameChecking
-NVR_NAVScripts\Export-NAVApplicationObject -Server $Server -Database $Database -Path $ResultFob -Filter 'Compiled=1' -NavIde $NavIde -LogFolder LOG
+
+NVR_NAVScripts\Export-NAVApplicationObject -Server $Server -Database $Database -Path $ResultFob -Filter 'Compiled=1' -NavIde $NavIde -LogFolder $LogFolder
 
