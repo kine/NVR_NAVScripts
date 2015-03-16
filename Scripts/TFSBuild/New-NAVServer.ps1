97999cb35ff5ed8a4ddbd6aafbea16ea35ca2b0e
KSACEK <kamil.sacek@navertica.com>
2015-03-16 10:45:16 +0100

    Forcing creating new NAV Server instance and DB in TFSBUILD\New-NAVServer script.

--- Scripts/TFSBuild/New-NAVServer.ps1
+++ Scripts/TFSBuild/New-NAVServer.ps1
@@ -5,7 +5,8 @@ Param (
 	[String]$TargetPath,
 	[String]$Server,
 	[String]$Database,
-	[String]$Instance
+	[String]$Instance,
+    [Bool]$ForceNewDB=$false
 )
 #Write-Host $PSScriptRoot
 if (Test-Path $TargetPath\src\setup.xml) {
@@ -15,8 +16,12 @@ Import-Module CommonPSFunctions -DisableNameChecking
 Import-Module NVR_NAVScripts -DisableNameChecking
 Import-NAVAdminTool
 
-if (Get-NAVServerInstance -ServerInstance $Instance) {
+if ((Get-NAVServerInstance -ServerInstance $Instance) -and (-not $ForceNewDB)) {
     Write-Host "Server instance $Instance already exists, skipping the initialization of environment..."
 } else {
+    if (Get-NAVServerInstance -ServerInstance $Instance) 
+    {
+        Remove-NAVLocalApplication -Server $Server -Database $Database -ServerInstance $Instance
+    }
     New-NAVLocalApplication -Server $Server -Database $Database -BaseFob $BaseFob -License $LicenseFile -DbBackupFile $DbBackupFile -ServerInstance $Instance -TargetPath $TargetPath
 }
