d57a09da9120e907f7fb7ab38f9c1678814f02e4
KSACEK <kamil.sacek@navertica.com>
2015-05-18 16:04:05 +0200

    Added progress info

--- Scripts/TFSBuild/Compile-NAVObjectsOnServer.ps1
+++ Scripts/TFSBuild/Compile-NAVObjectsOnServer.ps1
@@ -9,10 +9,12 @@ Param (
 
 Import-Module NVR_NAVScripts -Force -DisableNameChecking
 $ProgressPreference="SilentlyContinue"
+Write-Host 'Compiling uncompiled system tables...'
 Compile-NAVApplicationObject2 -DatabaseServer $Server -DatabaseName $Database -Filter 'Type=Table;Id=2000000000..' -LogPath $LogFolder -SynchronizeSchemaChanges Force
 #Preventing the error about "Must be compiled..."
+Write-Host 'Compiling uncompiled menusuites...'
 Compile-NAVApplicationObject2 -DatabaseServer $Server -DatabaseName $Database -Filter 'Type=MenuSuite;Compiled=0' -LogPath $LogFolder -SynchronizeSchemaChanges Force
-
+Write-Host 'Compiling rest of objects...'
 if ($CompileAll -eq 1) {
 #	Compile-NAVApplicationObjectFilesMulti -Files $Files -Server $Server -Database $Database -LogFolder $LogFolder -NavIde $NavIde
 	Compile-NAVApplicationObjectMulti -Server $Server -Database $Database -Filter 'Compiled=0|1'-LogFolder $LogFolder -NavIde $NavIde  -SynchronizeSchemaChanges Force -AsJob
