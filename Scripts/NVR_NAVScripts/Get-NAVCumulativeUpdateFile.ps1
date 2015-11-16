83c797faf8a4f2f32bc844b96dcfff72ccf27924
KSACEK <kamil.sacek@navertica.com>
2015-11-16 08:42:27 +0100

    fix(Get-CU): Updated Get-NAVCumulativeUpdate function

--- Scripts/NVR_NAVScripts/Get-NAVCumulativeUpdateFile.ps1
+++ Scripts/NVR_NAVScripts/Get-NAVCumulativeUpdateFile.ps1
@@ -1,138 +1,203 @@
 ﻿#requires -Version 3 -Modules BitsTransfer
+
+<#
+    .Synopsis
+    Download the Cumulative Update File for specified NAV version and localization
+    .DESCRIPTION
+    Download the Cumulative Update File for specified NAV version and localization, with possibility to 
+    select specific cumulative update
+    .EXAMPLE
+    Get-NAVCumulativeUpdateFile -langcodes 'intl','CSY' -versions '2013 R2','2015','2016'
+    .EXAMPLE
+    Get-NAVCumulativeUpdateFile -langcodes 'CSY' -versions '2013 R2' -CUNo 23
+    .OUTPUT
+    Objects with info about the downloaded cumulative updates
+#>
 function Get-NAVCumulativeUpdateFile
 {
     param (
-        [string]$langcode = 'intl',
-        [string]$version = '2016',
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string[]]$langcodes = 'intl',
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string[]]$versions = '2016',
+        [Parameter(ValueFromPipelineByPropertyName)]
         [string]$CUNo = ''
     )
 
-    Write-Host -Object 'Searching for RSS item' -ForegroundColor Green
-
-    $feed = [xml](Invoke-WebRequest -Uri 'https://blogs.msdn.microsoft.com/nav/category/announcements/cu/feed/')
-
-    if ($CUNo -gt '') {
-        $blogurl = $feed.SelectNodes("/rss/channel/item[./category='NAV $version' and ./category='Cumulative Updates' and contains(./title,'$CUNo')]").link
-    } else {
-        $blogurl = $feed.SelectNodes("/rss/channel/item[./category='NAV $version' and ./category='Cumulative Updates']").link
+    begin {
+        $ie = New-Object -ComObject 'internetExplorer.Application'
+        $ie.Visible = $true
     }
-
-    if (!$blogurl) {
-        Write-Error 'Blog url not found!'
-        return
-    }
-
-    Write-Host -Object "Reading blog page $blogurl" -ForegroundColor Green
-    
-    $blogarticle = Invoke-WebRequest -Uri $blogurl
-    
-    Write-Host -Object 'Searching for KB link' -ForegroundColor Green
-
-    $kblink = $blogarticle.Links | Where-Object -FilterScript {
-        $_.innerText -match 'KB'
-    }
-
-    $ie = New-Object -ComObject 'internetExplorer.Application'
-    $ie.Visible = $true
-    Write-Host -Object "Opening KB link $($kblink.href)" -ForegroundColor Green
-    $ie.Navigate($kblink.href)
-    while ($ie.Busy -eq $true)
+    process
     {
-        Start-Sleep -Seconds 1
-    }
-
-    if ($ie.LocationURL -match 'login.live.com') {
-        Write-Host 'Please, login. Script will continue automatically...' -ForegroundColor Magenta
-        while ($ie.LocationURL -match 'login.live.com') {
-            Start-sleep -Seconds 1
-        }
-    }
-    
-    if ($ie.LocationURL -match 'https://corp.sts.microsoft.com') 
-    {
-        Write-Host -Object 'Trying to login' -ForegroundColor Green
-        $loginlink = $ie.Document.getElementById('CustomHRD_LinkButton_LiveId')
-        Write-Host -Object 'Clicking the link to login' -ForegroundColor Green
-        $loginlink.click()
-        while ($ie.Busy -eq $true)
+        foreach ($langcode in $langcodes) 
         {
-            Start-Sleep -Seconds 1
-        }
-    }
+            foreach ($version in $versions) 
+            {
+                $url = ''
+                
+                Write-Host -Object "Processing parameters $langcode $version $CUNo" -ForegroundColor Cyan
+        
+                $feedurl = 'https://blogs.msdn.microsoft.com/nav/category/announcements/cu/feed/'
+                [regex]$titlepattern = 'Cumulative Update (\d+) .*'
 
-    if ($ie.LocationURL -match 'https://mbs2.microsoft.com/UserInfo/SelectProfile.aspx') 
-    {
-        Write-Host -Object 'Searching for identity selection radiobuttons' -ForegroundColor Green
-        $radiobuttons = $ie.Document.body.getElementsByTagName('input') | Where-Object -FilterScript {$_.type -eq 'radio' -and $_.name -eq 'radioGroup' }
-        Write-Host -Object 'Clicking first radio button' -ForegroundColor Green
-        $radiobuttons[0].setActive()
-        $radiobuttons[0].click()
-        $ie.Document.IHTMLDocument3_getElementsByName('continueButton')[0].click()
-        while ($ie.Busy -eq $true)
-        {
-            Start-Sleep -Seconds 1
-        }
-    }
+                Write-Host -Object 'Searching for RSS item' -ForegroundColor Green
 
-    Write-Host -Object 'Searching for download link' -ForegroundColor Green
-    $downloadlink = $ie.Document.links | Where-Object -FilterScript {
-        $_.id -match 'kb_hotfix_link'
-    }
-    Write-Host -Object "Opening download link $($downloadlink.href)" -ForegroundColor Green
-    $ie.Navigate($downloadlink.href)
-    while ($ie.Busy -eq $true)
-    {
-        Start-Sleep -Seconds 1
-    }
+                $feed = [xml](Invoke-WebRequest -Uri $feedurl)
 
-    Write-Host -Object 'Searching for Accept button' -ForegroundColor Green
+                if ($CUNo -gt '') 
+                {
+                    $blogurl = $feed.SelectNodes("/rss/channel/item[./category='NAV $version' and ./category='Cumulative Updates' and contains(./title,'$CUNo')]").link | Select-Object -First 1
+                } else 
+                {
+                    $blogurl = $feed.SelectNodes("/rss/channel/item[./category='NAV $version' and ./category='Cumulative Updates']").link | Select-Object -First 1
+                }
 
-    $button = $ie.Document.IHTMLDocument3_getElementsByName('accept')
+                if (!$blogurl) 
+                {
+                    Write-Error -Message 'Blog url not found!'
+                    return
+                }
 
-    if ($button) 
-    {
-        Write-Host -Object 'Clicking Accept button' -ForegroundColor Green
-        $button.click()
-        while ($ie.Busy -eq $true)
-        {
-            Start-Sleep -Seconds 1
-        }   
-    }
-
-    Write-Host -Object 'Searching for list of updates' -ForegroundColor Green
-
-    [regex]$pattern = 'hfList = (\[.+\}\])'
-    $matches = $pattern.Matches($ie.Document.body.innerText) 
-    if (!$matches) 
-    {
-        Write-Error -Message 'list of hotfixes not found!'
-        return
-    } 
-
-    Write-Host -Object 'Converting Json with updates' -ForegroundColor Green
-    $hotfixes = $matches.Groups[1].Value.Replace('\x','') | ConvertFrom-Json
-
-    $ie.Quit()
+                Write-Host -Object "Reading blog page $blogurl" -ForegroundColor Green
+                
+                $blogarticle = ''
+                $blogarticle = Invoke-WebRequest -Uri $blogurl
     
-    #URL examples:
-    #http://hotfixv4.microsoft.com/Dynamics NAV 2016/latest/W1KB3106089/43402/free/488130_intl_i386_zip.exe
-    #http://hotfixv4.microsoft.com/Dynamics NAV 2015/latest/CZKB3106088/43389/free/488059_CSY_i386_zip.exe
+                Write-Host -Object 'Searching for KB link' -ForegroundColor Green
+                
+                $titlematches = $titlepattern.Matches($blogarticle.ParsedHtml.title) 
+                $updateno = $titlematches.Groups[1]
 
-    Write-Host -Object "Searching for update for language $langcode" -ForegroundColor Green
+                $kblink = $blogarticle.Links | Where-Object -FilterScript {
+                    $_.innerText -match 'KB'
+                }
 
-    $hotfix = $hotfixes | Where-Object -FilterScript {
-        $_.langcode -eq $langcode
+                Write-Host -Object "Opening KB link $($kblink.href)" -ForegroundColor Green
+                $null = $ie.Navigate($kblink.href)
+                while ($ie.Busy -eq $true)
+                {
+                    $null = Start-Sleep -Seconds 1
+                }
+
+                if ($ie.LocationURL -match 'https://corp.sts.microsoft.com') 
+                {
+                    Write-Host -Object 'Trying to login' -ForegroundColor Green
+                    $loginlink = $ie.Document.IHTMLDocument3_getElementById('CustomHRD_LinkButton_LiveID')
+                    Write-Host -Object 'Clicking the link to login' -ForegroundColor Green
+                    $loginlink.click()
+                    while ($ie.Busy -eq $true)
+                    {
+                        $null = Start-Sleep -Seconds 1
+                    }
+                }
+
+                if ($ie.LocationURL -match 'login.live.com') 
+                {
+                    Write-Host -Object 'Please, login. Script will continue automatically...' -ForegroundColor Magenta
+                    while ($ie.LocationURL -match 'login.live.com') 
+                    {
+                        $null = Start-Sleep -Seconds 1
+                    }
+                }
+                while ($ie.Busy -eq $true)
+                {
+                    $null = Start-Sleep -Seconds 1
+                }   
+
+    
+                if ($ie.LocationURL -match 'https://mbs2.microsoft.com/UserInfo/SelectProfile.aspx') 
+                {
+                    Write-Host -Object 'Searching for identity selection radiobuttons' -ForegroundColor Green
+                    $radiobuttons = $ie.Document.body.getElementsByTagName('input') | Where-Object -FilterScript {
+                        $_.type -eq 'radio' -and $_.name -eq 'radioGroup' 
+                    }
+                    Write-Host -Object 'Clicking first radio button' -ForegroundColor Green
+                    $null = $radiobuttons[0].setActive()
+                    $null = $radiobuttons[0].click()
+                    $null = $ie.Document.IHTMLDocument3_getElementsByName('continueButton')[0].click()
+                    while ($ie.Busy -eq $true)
+                    {
+                        $null = Start-Sleep -Seconds 1
+                    }
+                }
+
+                Write-Host -Object 'Searching for download link' -ForegroundColor Green
+                $downloadlink = $ie.Document.links | Where-Object -FilterScript {
+                    $_.id -match 'kb_hotfix_link'
+                }
+                Write-Host -Object "Opening download link $($downloadlink.href)" -ForegroundColor Green
+                $null = $ie.Navigate($downloadlink.href)
+                while ($ie.Busy -eq $true)
+                {
+                    $null = Start-Sleep -Seconds 1
+                }
+
+                Write-Host -Object 'Searching for Accept button' -ForegroundColor Green
+
+                $button = $ie.Document.IHTMLDocument3_getElementsByName('accept')
+
+                if ($button.id) 
+                {
+                    Write-Host -Object 'Clicking Accept button' -ForegroundColor Green
+                    $null = $button.click()
+                    while ($ie.Busy -eq $true)
+                    {
+                        $null = Start-Sleep -Seconds 1
+                    }   
+                }
+
+                Write-Host -Object 'Searching for list of updates' -ForegroundColor Green
+
+                [regex]$pattern = 'hfList = (\[.+\}\])'
+                $matches = $pattern.Matches($ie.Document.body.innerText) 
+                if (!$matches) 
+                {
+                    Write-Error -Message 'list of hotfixes not found!'
+                    return
+                } 
+
+                Write-Host -Object 'Converting Json with updates' -ForegroundColor Green
+                $hotfixes = $matches.Groups[1].Value.Replace('\x','') | ConvertFrom-Json
+
+                #URL examples:
+                #http://hotfixv4.microsoft.com/Dynamics NAV 2016/latest/W1KB3106089/43402/free/488130_intl_i386_zip.exe
+                #http://hotfixv4.microsoft.com/Dynamics NAV 2015/latest/CZKB3106088/43389/free/488059_CSY_i386_zip.exe
+
+                Write-Host -Object "Searching for update for language $langcode" -ForegroundColor Green
+
+                $hotfix = $hotfixes | Where-Object -FilterScript {
+                    $_.langcode -eq $langcode
+                }
+
+                Write-Host -Object 'Creating hotfix URL' -ForegroundColor Green
+
+                $url = "http://hotfixv4.microsoft.com/$($hotfix.product)/$($hotfix.release)/$($hotfix.filename)/$($hotfix.build)/free/$($hotfix.fixid)_$($hotfix.langcode)_i386_zip.exe"
+
+                Write-Host -Object "Hotfix URL is $url" -ForegroundColor Green
+
+                $filename = (Join-Path -Path $env:TEMP -ChildPath "$($hotfix.fixid)_$($hotfix.langcode)_i386_zip.exe")
+                Write-Host -Object "Downloading hotfix to $filename" -ForegroundColor Green
+    
+                if (-not (Test-Path $filename)) 
+                {
+                    $null = Start-BitsTransfer -Source $url -Destination $filename
+                }
+    
+                Write-Host -Object 'Hotfix downloaded' -ForegroundColor Green
+                $null = Unblock-File -Path $filename
+
+                $result = New-Object -TypeName System.Object
+                $null = $result | Add-Member -TypeName NoteProperty -Name filename -Value "$filename"
+                $null = $result | Add-Member -TypeName NoteProperty -Name version -Value "$version"
+                $null = $result | Add-Member -TypeName NoteProperty -Name CUNo -Value "$updateno"
+                $null = $result | Add-Member -TypeName NoteProperty -Name langcode -Value "$langcode"
+        
+                Write-Output -InputObject $result
+            }
+        }        
     }
-
-    Write-Host -Object 'Creating hotfix URL' -ForegroundColor Green
-
-    $url = "http://hotfixv4.microsoft.com/$($hotfix.product)/$($hotfix.release)/$($hotfix.filename)/$($hotfix.build)/free/$($hotfix.fixid)_$($hotfix.langcode)_i386_zip.exe"
-
-    Write-Host -Object "Hotfix URL is $url" -ForegroundColor Green
-
-    $filename = (Join-Path -Path $env:TEMP -ChildPath "$($hotfix.fixid)_$($hotfix.langcode)_i386_zip.exe")
-    Write-Host -Object "Downloading hotfix to $filename" -ForegroundColor Green
-    Start-BitsTransfer -Source $url -Destination $filename
-    Write-Host -Object 'Hotfix downloaded' -ForegroundColor Green
-    return Get-Item $filename
+    end {
+        $null = $ie.Quit()
+    }
 }
