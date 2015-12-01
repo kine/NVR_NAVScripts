a2b12c26ba469218100fefb7148fafc2d9e5ae3b
KSACEK <kamil.sacek@navertica.com>
2015-12-01 08:16:48 +0100

    feat(install): Added install script

---
+++ Scripts/Install-Modules.ps1
@@ -0,0 +1,19 @@
+﻿#requires -Version 1
+#requires -runasadministrator
+
+$installfolder = $PSScriptRoot
+
+if (-not (([Environment]::GetEnvironmentVariable('PSModulePath','Machine')) -like "*$installfolder*")) 
+{
+    Write-Host -Object "Extending PSModulePath with $PSScriptRoot" -ForegroundColor Green
+    $env:PSModulePath = $env:PSModulePath + ';' + $installfolder
+    Write-Host -Object "Extending Computer wide PSModulePath with $PSScriptRoot" -ForegroundColor Green
+    [Environment]::SetEnvironmentVariable('PSModulePath',[Environment]::GetEnvironmentVariable('PSModulePath','Machine')+';'+$installfolder,'Machine')
+
+    Write-Host 'Importing the modules' -ForegroundColor Green
+    Import-Module CommonPSFunctions -DisableNameChecking -Global
+    Import-Module NVR_NAVScripts -DisableNameChecking -Global
+} else 
+{
+    Write-Host -Object "PSModulePath already includes $PSScriptRoot, skipping the setting" -ForegroundColor Green
+}
