8ac4b9fe82e9889799b4f8ac7e291ec2d8a3197b
KSACEK <kamil.sacek@navertica.com>
2015-12-16 16:54:08 +0100

    chore(Git): remove .exe extension when calling git

--- Scripts/NVR_GitScripts/Commit-GitRepository.ps1
+++ Scripts/NVR_GitScripts/Commit-GitRepository.ps1
@@ -13,11 +13,11 @@
     #$result = git config --global user.email "$email" 
     $email = Get-UserEmail -UserName $Author
     Write-Host 'Adding all into git' -ForegroundColor Green
-    $result = git.exe add --all
+    $result = git add --all
     Write-Host 'Commiting changes to git' -ForegroundColor Green
-    $result = git.exe commit --message="$CommitMessage" --all --quiet --author="$Author <$email>"
+    $result = git commit --message="$CommitMessage" --all --quiet --author="$Author <$email>"
     Write-Host 'Pushing all to remote' -ForegroundColor Green
     #$result = git.exe push --quiet --all --porcelain | Out-Null
     Write-Host 'Fetching all from remote' -ForegroundColor Green
-    $result = git.exe fetch --quiet --all
+    $result = git fetch --quiet --all
 }
