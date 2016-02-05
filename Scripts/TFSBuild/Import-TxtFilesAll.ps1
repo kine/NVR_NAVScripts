778b1d5da4a6e4b3d9a87a71a6ef4df2278211c3
KSACEK <kamil.sacek@navertica.com>
2016-02-05 12:49:39 +0100

    feat(TFS): Add Import-TxtFilesAll to tfs scripts to be used to force import all

---
+++ Scripts/TFSBuild/Import-TxtFilesAll.ps1
@@ -0,0 +1,22 @@
+﻿param (
+    #Object files from which to update. Should be complete set of objects
+    [Parameter(Mandatory=$true,ValueFromPipelinebyPropertyName=$True)]
+    [String]$Files,
+    #SQL Server address
+    [Parameter(Mandatory=$true,ValueFromPipelinebyPropertyName=$True)]
+    [String]$Server,
+    #SQL Database to update
+    [Parameter(Mandatory=$true,ValueFromPipelinebyPropertyName=$True)]
+    [String]$Database,
+    #LogFolder
+    [Parameter(Mandatory=$true,ValueFromPipelinebyPropertyName=$True)]
+    [String]$LogFolder
+)
+
+Import-Module -Name NVR_NAVScripts -DisableNameChecking -Force
+Import-Module -Name CommonPSFunctions
+Import-Module (Get-NAVAdminModuleName)
+
+$ProgressPreference="SilentlyContinue"
+Update-NAVApplicationFromTxt -Files $Files -Server $Server -Database $Database -LogFolder $LogFolder -MarkToDelete -NoProgress -All
+$ProgressPreference="Continue"
