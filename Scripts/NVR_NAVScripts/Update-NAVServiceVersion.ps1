e6bc999c2c61b6303e98163d4731790f7e42db83
KSACEK <kamil.sacek@navertica.com>
2016-10-18 09:04:09 +0200

    chore(Update): Tracing more info

--- Scripts/NVR_NAVScripts/Update-NAVServiceVersion.ps1
+++ Scripts/NVR_NAVScripts/Update-NAVServiceVersion.ps1
@@ -22,10 +22,12 @@
         Write-InfoMessage 'Getting current path for the service...'
         $currentPath = (Get-ItemProperty -Path ('HKLM:\System\CurrentControlSet\Services\'+$service.Name) -Name ImagePath).ImagePath
         $originalPath = (Split-Path $currentPath.Split('$')[0]).Replace('"','').Replace('''','')
+        Write-InfoMessage "Original path: $originalPath"
     
         Write-InfoMessage 'Getting target path...'
         $TargetFolder = Find-NAVVersion -path $originalPath -Version $Version
         $OldNavIdePath = $env:NAVIdePath
+        Write-InfoMessage "Target path: $TargetFolder"
     
         if ($TargetFolder -eq $originalPath) {
             Write-InfoMessage 'Path is same. No change or version not found...'
