d516415d8a35cd601285416c0bc54fc9d62bcde0
KSACEK <kamil.sacek@navertica.com>
2016-12-01 15:59:05 +0100

    chore(MultiVersion): Function Find-NAVVersion moved on top to exists first
    Whenloading subscripts the function was missing thus leading to errors
    about Find-NAVVersion not found...

--- Scripts/NVR_NAVScripts/NVR_NAVScripts.psm1
+++ Scripts/NVR_NAVScripts/NVR_NAVScripts.psm1
@@ -5,6 +5,63 @@
 #Import-NAVAdminTool
 #Import-NAVModelTool
 
+<#
+        .Synopsis
+        Try to find specified version of NAV in folders on same level as passed default path
+        .DESCRIPTION
+        Return folder name for selected NAV version. If not found, return the passed default path
+        .EXAMPLE
+        Find-NAVVersion 'C:\Program Files (x86)\Microsoft Dynamics NAV\80\RoleTailored Client' '8.0.40262.0'
+        .EXAMPLE
+        Find-NAVVersion 'C:\Program Files\Microsoft Dynamics NAV\80\Service' '8.0.40262.0'
+#>
+
+function Find-NAVVersion
+{
+    param
+    (
+        #Default path, where to start the search
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Default path, where to start the search')]
+        $path,
+        #Version which we are looking for
+        [Parameter(Mandatory = $true, Position = 1, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Version which we are looking for')]
+        $Version
+    )
+    if (!($Version)) {
+        return $path
+    }
+    
+    if ($path -like '*.exe') {
+        $filename = (Split-Path -Path $path -Leaf)
+        $path = (Split-Path -Path $path)
+    }
+    if (Test-Path -Path (Join-Path -Path $path -ChildPath 'Microsoft.Dynamics.Nav.Server.exe')) 
+    {
+        $searchfile = 'Microsoft.Dynamics.Nav.Server.exe'
+    }
+    if (Test-Path -Path (Join-Path -Path $path -ChildPath 'finsql.exe')) 
+    {
+        $searchfile = 'finsql.exe'
+    } 
+    Write-InfoMessage "Searching for version $Version in $(Split-Path (Split-Path $path))"
+    $result = Split-Path (Split-Path $path) |
+    Get-ChildItem -Filter $searchfile -Recurse |
+    Where-Object -FilterScript {
+        $_.VersionInfo.FileVersion -eq $Version
+    } 
+    if ($result) 
+    {
+        if ($result.Count -gt 1) {
+            Write-InfoMessage "Found $($result[0].DirectoryName)"
+            return (Join-Path $result[0].DirectoryName $filename)
+        }
+        Write-InfoMessage "Found $($result.DirectoryName)"
+        return (Join-Path $result.DirectoryName $filename)
+    }
+    Write-InfoMessage "Not found, returning $path"
+    return (Join-Path $path $filename)
+}
+
 Get-Item $PSScriptRoot  | Get-ChildItem -Recurse -file -Filter '*.ps1' |  Sort Name | foreach {
     Write-Verbose "Loading $($_.Name)"  
     . $_.fullname
@@ -1044,63 +1101,6 @@ function Get-GITModifiedFile
     $list = (git.exe diff --name-only "$FromCommit" "$ToCommit")
     Pop-Location
     return $list
-}
-
-<#
-        .Synopsis
-        Try to find specified version of NAV in folders on same level as passed default path
-        .DESCRIPTION
-        Return folder name for selected NAV version. If not found, return the passed default path
-        .EXAMPLE
-        Find-NAVVersion 'C:\Program Files (x86)\Microsoft Dynamics NAV\80\RoleTailored Client' '8.0.40262.0'
-        .EXAMPLE
-        Find-NAVVersion 'C:\Program Files\Microsoft Dynamics NAV\80\Service' '8.0.40262.0'
-#>
-
-function Find-NAVVersion
-{
-    param
-    (
-        #Default path, where to start the search
-        [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Default path, where to start the search')]
-        $path,
-        #Version which we are looking for
-        [Parameter(Mandatory = $true, Position = 1, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Version which we are looking for')]
-        $Version
-    )
-    if (!($Version)) {
-        return $path
-    }
-    
-    if ($path -like '*.exe') {
-        $filename = (Split-Path -Path $path -Leaf)
-        $path = (Split-Path -Path $path)
-    }
-    if (Test-Path -Path (Join-Path -Path $path -ChildPath 'Microsoft.Dynamics.Nav.Server.exe')) 
-    {
-        $searchfile = 'Microsoft.Dynamics.Nav.Server.exe'
-    }
-    if (Test-Path -Path (Join-Path -Path $path -ChildPath 'finsql.exe')) 
-    {
-        $searchfile = 'finsql.exe'
-    } 
-    Write-InfoMessage "Searching for version $Version in $(Split-Path (Split-Path $path))"
-    $result = Split-Path (Split-Path $path) |
-    Get-ChildItem -Filter $searchfile -Recurse |
-    Where-Object -FilterScript {
-        $_.VersionInfo.FileVersion -eq $Version
-    } 
-    if ($result) 
-    {
-        if ($result.Count -gt 1) {
-            Write-InfoMessage "Found $($result[0].DirectoryName)"
-            return (Join-Path $result[0].DirectoryName $filename)
-        }
-        Write-InfoMessage "Found $($result.DirectoryName)"
-        return (Join-Path $result.DirectoryName $filename)
-    }
-    Write-InfoMessage "Not found, returning $path"
-    return (Join-Path $path $filename)
 }
 
 Function Set-NAVUIDOffset
