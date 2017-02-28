561583de32846eb6f1158ef71be5b28fe000b9bf
KSACEK <kamil.sacek@navertica.com>
2017-02-28 15:07:52 +0100

    fix(Merge): NAV 2017 version list support in merge-navversionliststring
    Added support for NAV 2017 when merging version lists

--- Scripts/NVR_NAVScripts/NVR_NAVScripts.psm1
+++ Scripts/NVR_NAVScripts/NVR_NAVScripts.psm1
@@ -119,6 +119,25 @@ function Get-VersionListHash
     return $hash
 }
 
+function Get-NAVVersionListBigger
+{
+    param
+    (
+        [System.String]$list1='',
+        [System.String]$list2=''
+    )
+    $l1 = $list1.IndexOf('.')
+    $l2 = $list2.IndexOf('.')
+    $l = [math]::Max($l1,$l2)
+
+    $list1b = $list1.PadLeft($list1.Length+$l-$l1,'0')
+    $list2b = $list2.PadLeft($list2.Length+$l-$l2,'0')
+    if ($list1b -ge $list2b) {
+        return $list1
+    } else {
+        return $list2
+    }
+}
 function Merge-NAVVersionListString 
 {
     param
@@ -152,14 +171,8 @@ function Merge-NAVVersionListString
     {
         $actualversion = ''
         $moduleinfo = Get-VersionListModuleShortcut($module)
-        if ($sourcehash[$moduleinfo.shortcut] -ge $targethash[$moduleinfo.shortcut]) 
-        {
-            $actualversion = $sourcehash[$moduleinfo.shortcut]
-        }
-        else 
-        {
-            $actualversion = $targethash[$moduleinfo.shortcut]
-        }
+        $actualversion = Get-NAVVersionListBigger $sourcehash[$moduleinfo.shortcut] $targethash[$moduleinfo.shortcut]
+
         if ($moduleinfo.shortcut -eq $newmoduleinfo.shortcut) 
         {
             $actualversion = $newmoduleinfo.version
