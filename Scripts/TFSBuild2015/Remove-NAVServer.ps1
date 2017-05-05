26785c571d8aeb8dc177a0c67b8b8dc9d60b8598
KSACEK <kamil.sacek@navertica.com>
2017-05-05 09:39:45 +0200

    feat(Remove): Remove script updated to take parameters from TFS

--- Scripts/TFSBuild2015/Remove-NAVServer.ps1
+++ Scripts/TFSBuild2015/Remove-NAVServer.ps1
@@ -1,14 +1,14 @@
 param(
-	[String]$Server,
-	[String]$Database,
-	[String]$Instance
+	[String]$NavIde=$env:NAV_NAVIDEPATH,
+	[String]$Server=$env:NAV_SQLSERVER,
+	[String]$Database=$env:NAV_SQLSERVERDB,
+	[String]$Instance=$env:NAV_SERVERINSTANCE
 )
-if (Test-Path $TargetPath\setup.xml) {
-    $null = (. "$TargetPath\NVRPowerShell\Scripts\Settings.ps1")
-    $config = (. "$TargetPath\NVRPowerShell\Scripts\Get-NAVGITSetup.ps1" -SetupFile "$TargetPath\setup.xml")
+if (Test-Path $env:BUILD_SOURCESDIRECTORY\setup.xml) {
+    $config = (. "$PSScriptRoot\..\Get-NAVGITSetup.ps1" -SetupFile "$env:BUILD_SOURCESDIRECTORY\setup.xml")
 }
 
-Import-Module -Name NVR_NAVScripts -DisableNameChecking -Force
-Import-Module -Name CommonPSFunctions
-Import-Module (Get-NAVAdminModuleName)
+Import-Module CommonPSFunctions -Force -DisableNameChecking
+Import-Module NVR_NAVScripts -Force -DisableNameChecking
+Import-Module (Get-NAVAdminModuleName -NAVVersion $config.NAVVersion) -Force
 Remove-NAVLocalApplication -Server $Server -Database $Database -ServerInstance $Instance
