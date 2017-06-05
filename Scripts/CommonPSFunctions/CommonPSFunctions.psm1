9e2a1eac0dddcaac8cfddd866521a476e0ff4ea2
KSACEK <kamil.sacek@navertica.com>
2017-06-05 15:37:04 +0200

    feat(AsAdmin): Add fucntion to elevate permissions

--- Scripts/CommonPSFunctions/CommonPSFunctions.psm1
+++ Scripts/CommonPSFunctions/CommonPSFunctions.psm1
@@ -605,6 +605,28 @@ function Test-Administrator
     return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
 
+function Invoke-AsAdministrator
+{
+    param(
+        #use $myInvocation.MyCommand.Definition to get current command to rerun
+        $Definition
+    )
+    # We are not running "as Administrator" - so relaunch as administrator
+   
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $Definition;
+  
+    # Indicate that the process should be elevated
+    $newProcess.Verb = 'runas';
+   
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+    # Exit from the current, unelevated, process
+    exit
+}
 function Set-InfoMessageVerbosity
 {
     [CmdletBinding()]
