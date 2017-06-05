9529839c4fb47c156c0189c30c9856f3f70cfe7a
KSACEK <kamil.sacek@navertica.com>
2017-06-05 17:11:11 +0200

    feat(SQL): Get default SQL path

---
+++ Scripts/CommonPSFunctions/Get-SQLDefaultDataPath.ps1
@@ -0,0 +1,9 @@
+﻿function Get-SQLDefaultDataPath
+{
+    param (
+        $Server
+    )
+    $Command= "SELECT [DefaultDataPath] = SERVERPROPERTY('InstanceDefaultDataPath')"
+    $Result = Get-SQLCommandResult -Server $Server -Database master -Command $Command
+    return $Result.DefaultDataPath
+}
