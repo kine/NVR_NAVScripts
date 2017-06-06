c71ae49c15d0d8748a0b2f5fbb820cb74006a170
KSACEK <kamil.sacek@navertica.com>
2017-06-06 16:48:18 +0200

    fix(GIT): Fixing git functions to run smoothly

--- Scripts/NVR_GitScripts/Commit-GitRepository.ps1
+++ Scripts/NVR_GitScripts/Commit-GitRepository.ps1
@@ -18,7 +18,7 @@
     Write-Host 'Commiting changes to git' -ForegroundColor Green
     $result = git commit --message="$CommitMessage" --all --quiet --author="$Author <$email>"
     Write-Host 'Pushing all to remote' -ForegroundColor Green
-    $result = git.exe push --quiet --all --porcelain | Out-Null
+    $result = git.exe push --quiet --porcelain | Out-Null
     Write-Host 'Fetching all from remote' -ForegroundColor Green
     $result = git fetch --quiet --all
     Pop-Location
--- Scripts/NVR_GitScripts/Update-GitRepository.ps1
+++ Scripts/NVR_GitScripts/Update-GitRepository.ps1
@@ -14,6 +14,13 @@
     Import-NAVModelTool -Global
     Get-GitBranch -Repository $Repository -Branch $Branch
     
+    if ($FileName.IndexOf('W1') -gt 0) {
+        (Get-Content $FileName -Encoding Oem) `
+        -replace '    Date=(\d{2})-(\d{2})-(\d{2});', '    Date=$1.$2.$3;' |
+        Out-File (Join-Path (Split-Path $FileName) "TR_$(Split-Path $FileName -Leaf)") -Encoding oem -Force
+        $FileName = (Join-Path (Split-Path $FileName) "TR_$(Split-Path $FileName -Leaf)")
+    }
+    
     $TargetPath = (Join-Path $Repository $Folder)
     Write-Host "Splitting $FileName to the repository $TargetPath" -ForegroundColor Green
     Split-NAVApplicationObjectFile -Source $FileName -Destination $TargetPath -Force -PreserveFormatting
